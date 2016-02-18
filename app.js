c48614204b394f6b4ef6adc41d86b7b25d2218fa
MezianeHadjadj <mezianeh3@gmail.com>
2016-02-18 17:44:24 +0100

    working on get_last_tweets feature

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', posts);
 app.use('/', linkedin_details);
 
-console.log("working on get_last_tweet feature branch");
+console.log("working on get_last_tweets feature from develo branch");
 
 var server = app.listen(5000, function(){
     console.log('Launching Twitter insights server on port 3000');
