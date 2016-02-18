369d34443bd8f189c5066d1e095f210c0d3d88a1
MezianeHadjadj <mezianeh3@gmail.com>
2016-02-18 18:16:00 +0100

    enhance code quality using jslint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* global require:false */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -38,7 +39,7 @@ app.set('view cache', false); // désactivation du cache express
 swig.setDefaults({ cache: false }); // désactivation du cache swig
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -49,7 +50,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -60,7 +61,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -69,7 +70,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-exports.closeServer = function(){
+exports.closeServer = function () {
     server.close();
 };
 
